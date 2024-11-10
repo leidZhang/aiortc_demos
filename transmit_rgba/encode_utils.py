3a5a5b1507548ea48f7d0a89bba654b59b04dca3
leidZhang <haoz28889@gmail.com>
2024-11-10 00:16:09 -0500

    rgb to rgba

--- transmit_rgba/encode_utils.py
+++ transmit_rgba/encode_utils.py
@@ -31,3 +31,13 @@ def decode_from_rgba(rgba: np.ndarray, data_type: np.dtype) -> np.ndarray:
         raise InvalidDataTypeError("Data type must be float32 or int32")
 
     return rgba.view(data_type).reshape(rgba.shape[0], rgba.shape[1], LABEL_MAP_CHANNELS)
+
+
+def rgb_to_rgba(rgb: np.ndarray, alpha: int = 255) -> np.ndarray:
+    if alpha < 0 or alpha > 255:
+        raise ValueError("Alpha channel must be between 0 and 255")
+    if rgb.shape[-1] != 3:
+        raise InvalidImageShapeError("RGB image must have 3 channels")
+
+    alpha_channel: np.ndarray = np.ones((rgb.shape[0], rgb.shape[1], 1), dtype=np.uint8) * alpha
+    return np.concatenate((rgb, alpha_channel), axis=-1)
