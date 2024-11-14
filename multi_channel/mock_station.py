47f6a4fc201b4a913f02f7779d1996a8a6b71229
leidZhang <haoz28889@gmail.com>
2024-11-14 02:57:40 -0500

    multi-streams

--- multi_channel/mock_station.py
+++ multi_channel/mock_station.py
@@ -17,48 +17,34 @@ class StationClient(WebRTCClient):
         self.data_channel_event: asyncio.Event = asyncio.Event()
         self.data_channel: RTCDataChannel = None
         self.media_relay: MediaRelay = MediaRelay()
+        self.track_counter: int = 0
 
     def __setup_track_callbacks(self) -> None:
         @self.pc.on("track")
         async def on_track(track: VideoStreamTrack):
             print("Track received", track.kind)
             if track.kind == "video":
-                while True:
+                current: int = self.track_counter
+                self.track_counter += 1
+                while not self.done.is_set():
                     frame: av.VideoFrame = await track.recv()
                     # Process the frame (e.g., display it using OpenCV)
                     image: np.ndarray = frame.to_ndarray(format="bgr24")
-                    cv2.imshow("Received Video", image)
-                    cv2.waitKey(1)
-
-    def __setup_datachannel_callbacks(self) -> None:
-        @self.pc.on("datachannel")
-        async def on_datachannel(channel: RTCDataChannel) -> None:
-            self.data_channel = channel
-
-            @self.data_channel.on("open")
-            async def on_open() -> None:
-                while True:
-                    await asyncio.sleep(0.03)
-                    self.data_channel.send("Data channel opened by Jackal")
-
-            @self.data_channel.on("message")
-            def on_message(message: str) -> None:
-                ...
-                # print(f"Received message: {message} from Jackal")
-                # self.data_channel.send("Hello from workstation")
-
-            @self.data_channel.on("close")
-            def on_close() -> None:
-                print("Data channel closed by Jackal")
-
-            # NOTE: I dont know why this is needed, but without it, on_open() is not called
-            if self.data_channel.readyState == "open":
-                await on_open()
+                    if current == 0:
+                        window_name: str = "Camera"
+                        print("Demonstrating camera")
+                        cv2.imshow(window_name, image)
+                        cv2.waitKey(1)
+                    elif current == 1:
+                        window_name: str = "Colored"
+                        print("Demonstrating colored")
+                    else:
+                        window_name: str = "Mosaic"
+                        print("Demonstrating mosaic")
 
     async def run(self) -> None:
         await super().run()
         self.__setup_track_callbacks()
-        self.__setup_datachannel_callbacks()
         await receive_signaling(self.pc, self.signaling)
 
         await self.done.wait()
